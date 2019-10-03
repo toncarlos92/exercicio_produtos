d0199ac0a95323efafc8606d62af2cea31787992
Antonio Carlos <antoniolp_92@hotmail.com>
2019-10-02 21:30:40 -0300

    Exercicio de produtos finalizado

---
+++ Enumeracoes_Composicoes/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enumeracoes_Composicoes.Entities
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
